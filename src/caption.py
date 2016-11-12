8cab0ca88ff0ee9e71f0361ccd3f60e528ecef8e
Parham Pourdavood <ppourdavood@gmail.com>
2016-11-12 03:12:42 -0800

    create a module to caption images
    implement a fucntion named caption that takes image URL and an API key and sends a request to
    Microsoft's Computer Vision API to receive a JSON that includes the caption for the given image.

--- src/caption.py
+++ src/caption.py
@@ -12,4 +12,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import httplib, urllib, base64, ast, json
 
+def caption(image_src, api_key):
+	"""
+	It takes an image URL and an API key to send a request to Microsoft Computer Vision API
+	and returns a caption(string) for the image. 
+
+	Parameters
+	----------
+	image_src:		str
+	api_key:		str
+	                Microsoft's api_key
+
+	Returns
+	-------
+	captioned_data:	str
+	"""
+	headers = {
+		# Request headers
+		'Content-Type': 'application/json',
+		'Ocp-Apim-Subscription-Key': api_key,
+	}
+
+	params = urllib.urlencode({
+		# Request parameters
+		'maxCandidates': '1',
+	})
+
+    #data is the data that takes the image source. It is converted to str using a json method
+	data = json.dumps({"Url": image_src}, separators=(',',':'))
+
+
+	try:
+		conn = httplib.HTTPSConnection('api.projectoxford.ai')
+		conn.request("POST", "/vision/v1.0/describe?%s" % params, data, headers)
+		response = conn.getresponse()
+		data = ast.literal_eval(response.read())
+		#captioned_data is a JSON that we need to navigate in order to get to the caption text.
+		captioned_data = data['description']['captions'][0]["text"]
+		return(captioned_data)
+		conn.close()
+	except Exception as e:
+		print("[Errno {0}] {1}")
